8339bd3fb66becf5192c618c5457c0d2626b6a16
Li Chun <gengyuntuo@163.com>
2017-02-27 09:15:10 +0800

    项目初始化

---
+++ src/main/java/cn/xuemengzihe/util/excel/GenerateExcelFile.java
@@ -0,0 +1,36 @@
+package cn.xuemengzihe.util.excel;
+
+import java.io.File;
+import java.util.List;
+
+/**
+ * <h1>生成Excel文件</h1>
+ * <p>
+ * </p>
+ * 
+ * @author 李春
+ * @time 2017年2月23日 下午9:49:49
+ */
+public class GenerateExcelFile {
+	/**
+	 * @param bookName
+	 *            Excel 文件名
+	 * @param sheetName
+	 *            工作表名称
+	 * @param sheetTitle
+	 *            工作表标题
+	 * @param ColumnTitles
+	 *            工作表列名
+	 * @param list
+	 *            工作表内容
+	 * @param type
+	 *            工作表POJO类型
+	 * @return
+	 */
+	public File generateExcelFile(String bookName, String sheetName,
+			String sheetTitle, List<String> ColumnTitles, List<Object> list,
+			Class<Object> type) {
+		;
+		return null;
+	}
+}
