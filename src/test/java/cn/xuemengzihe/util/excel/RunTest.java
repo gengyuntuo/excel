eb1951fcc0958bb4a45c893fde1a096dc5903c07
Li Chun <gengyuntuo@163.com>
2017-03-27 17:38:47 +0800

    修改

--- src/test/java/cn/xuemengzihe/util/excel/RunTest.java
+++ src/test/java/cn/xuemengzihe/util/excel/RunTest.java
@@ -1,23 +1,20 @@
 package cn.xuemengzihe.util.excel;
 
-import java.io.File;
-import java.io.IOException;
-
 import org.junit.Test;
 
 public class RunTest {
 
 	@Test
 	public void test() {
-		File file = new File("workbook3.xls");
-		ParseExcelFile excel = new ParseExcelFile(file);
-		System.out.println(excel.getSheetTitle(0));
-		System.out.println(excel.getColumnNames(0));
-		System.out.println(excel.getSheetContent(0));
-		try {
-			excel.close();
-		} catch (IOException e) {
-		}
+		// File file = new File("workbook3.xls");
+		// ParseExcelFile excel = new ParseExcelFile(file);
+		// System.out.println(excel.getSheetTitle(0));
+		// System.out.println(excel.getColumnNames(0));
+		// System.out.println(excel.getSheetContent(0));
+		// try {
+		// excel.close();
+		// } catch (IOException e) {
+		// }
 	}
 
 }
